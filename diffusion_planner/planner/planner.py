98f69f95640311b863ed72ae1e76257fc09f5df6
ruiwan <ruiwan@deeproute.ai>
2025-03-12 10:59:24 +0000

    update vis tools

--- diffusion_planner/planner/planner.py
+++ diffusion_planner/planner/planner.py
@@ -20,12 +20,23 @@ from nuplan.planning.simulation.planner.abstract_planner import (
 from diffusion_planner.model.diffusion_planner import Diffusion_Planner
 from diffusion_planner.data_process.data_processor import DataProcessor
 from diffusion_planner.utils.config import Config
-
+from pathlib import Path
+from nuplan.planning.scenario_builder.abstract_scenario import AbstractScenario
+from diffusion_planner.utils.nuplan_scenario_render import NuplanScenarioRender
+from diffusion_planner.utils.scenario_manager import ScenarioManager
+from nuplan.planning.simulation.planner.planner_report import MLPlannerReport
+from nuplan.planning.simulation.planner.abstract_planner import (
+    AbstractPlanner,
+    PlannerInitialization,
+    PlannerInput,
+    PlannerReport,
+)
 def identity(ego_state, predictions):
     return predictions
 
 
 class DiffusionPlanner(AbstractPlanner):
+    requires_scenario: bool = True
     def __init__(
             self,
             config: Config,
@@ -36,6 +47,11 @@ class DiffusionPlanner(AbstractPlanner):
 
             enable_ema: bool = True,
             device: str = "cpu",
+            render: bool = True,
+            save_dir: str = "/mnt/csi-data-aly/user/ruiwan/nuplan_repo/Diffusion-Planner/vis",
+            scenario: AbstractScenario = None,
+            eval_dt: float = 0.1,
+            eval_num_frames: int = 80,
         ):
 
         assert device in ["cpu", "cuda"], f"device {device} not supported"
@@ -56,9 +72,33 @@ class DiffusionPlanner(AbstractPlanner):
 
         self._planner = Diffusion_Planner(config)
 
+        self._initialization: Optional[PlannerInitialization] = None
+        self._scenario_manager: Optional[ScenarioManager] = None
+
+        self._eval_dt = eval_dt
+        self._eval_num_frames = eval_num_frames
+
+        # Runtime stats for the MLPlannerReport
+        self._feature_building_runtimes: List[float] = []
+        self._inference_runtimes: List[float] = []
+
+        self._scenario_type = scenario.scenario_type
+
         self.data_processor = DataProcessor(config)
         
         self.observation_normalizer = config.observation_normalizer
+
+        self._render = render
+        self._imgs = []
+        self._scenario = scenario
+        if render:
+            self._scene_render = NuplanScenarioRender()
+            if save_dir is not None:
+                self.video_dir = Path(save_dir)
+            else:
+                self.video_dir = Path(os.getcwd())
+            self.video_dir.mkdir(exist_ok=True, parents=True)
+
 
     def name(self) -> str:
         """
@@ -97,6 +137,15 @@ class DiffusionPlanner(AbstractPlanner):
         self._planner = self._planner.to(self._device)
         self._initialization = initialization
 
+        self._scenario_manager = ScenarioManager(
+            map_api=initialization.map_api,
+            ego_state=None,
+            route_roadblocks_ids=initialization.route_roadblock_ids,
+            radius=self._eval_dt * self._eval_num_frames * 60 / 4.0,
+        )
+        if self._render:
+            self._scene_render.scenario_manager = self._scenario_manager
+
     def planner_input_to_model_inputs(self, planner_input: PlannerInput) -> Dict[str, torch.Tensor]:
         history = planner_input.history
         traffic_light_data = list(planner_input.traffic_light_data)
@@ -114,6 +163,29 @@ class DiffusionPlanner(AbstractPlanner):
 
         return states
     
+    def _global_to_local(self, global_trajectory: np.ndarray, ego_state: EgoState):
+        if isinstance(global_trajectory, InterpolatedTrajectory):
+            states: List[EgoState] = global_trajectory.get_sampled_trajectory()
+            global_trajectory = np.stack(
+                [
+                    np.array(
+                        [state.rear_axle.x, state.rear_axle.y, state.rear_axle.heading]
+                    )
+                    for state in states
+                ],
+                axis=0,
+            )
+
+        origin = ego_state.rear_axle.array
+        angle = ego_state.rear_axle.heading
+        rot_mat = np.array(
+            [[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]]
+        )
+        position = np.matmul(global_trajectory[..., :2] - origin, rot_mat)
+        heading = global_trajectory[..., 2] - angle
+
+        return np.concatenate([position, heading[..., None]], axis=-1)
+    
     def compute_planner_trajectory(self, current_input: PlannerInput) -> AbstractTrajectory:
         """
         Inherited.
@@ -127,5 +199,53 @@ class DiffusionPlanner(AbstractPlanner):
             trajectory=self.outputs_to_trajectory(outputs, current_input.history.ego_states)
         )
 
+        ego_state = current_input.history.ego_states[-1]
+        self._scenario_manager.update_ego_state(ego_state)
+        self._scenario_manager.update_drivable_area_map()
+
+        if self._render:
+            print("yes we are rendering")
+            self._imgs.append(
+                self._scene_render.render_from_simulation(
+                    current_input=current_input,
+                    initialization=self._initialization,
+                    route_roadblock_ids=self._scenario_manager.get_route_roadblock_ids(),
+                    scenario=self._scenario,
+                    iteration=current_input.iteration.index,
+                    planning_trajectory=self._global_to_local(trajectory, ego_state),
+                    # candidate_trajectories=self._global_to_local(
+                    #     candidate_trajectories[rule_based_scores > 0], ego_state
+                    # ),
+                    # candidate_index=best_candidate_idx,
+                    # predictions=predictions,
+                    return_img=True,
+                )
+            )
+
         return trajectory
+    
+    def generate_planner_report(self, clear_stats: bool = True) -> PlannerReport:
+        """Inherited, see superclass."""
+        report = MLPlannerReport(
+            compute_trajectory_runtimes=self._compute_trajectory_runtimes,
+            feature_building_runtimes=self._feature_building_runtimes,
+            inference_runtimes=self._inference_runtimes,
+        )
+        if clear_stats:
+            self._compute_trajectory_runtimes: List[float] = []
+            self._feature_building_runtimes = []
+            self._inference_runtimes = []
+
+        if self._render:
+            import imageio
+
+            imageio.mimsave(
+                self.video_dir
+                / f"{self._scenario.log_name}_{self._scenario.token}.mp4",
+                self._imgs,
+                fps=10,
+            )
+            print("\n video saved to ", self.video_dir / "video.mp4\n")
+
+        return report
     
